7f91bb895a1584cd4aee13ba08d873ab056c4290
maksymbr <maksymb@1x-193-157-184-112.uio.no>
2019-09-25 14:10:30 +0200

    Back-up

--- RegressionPipeline.py
+++ RegressionPipeline.py
@@ -9,6 +9,7 @@ Created on Thu Sep 19 11:44:18 2019
 # Standard imports
 import os
 import numpy as np
+import random
 #import numpy.polynomial.polynomial as poly
 # 
 from mpl_toolkits.mplot3d import Axes3D
@@ -19,6 +20,9 @@ import sympy as sp
 #from sympy import *
 import itertools as it
 # Machine Learning libraries
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 import time
@@ -26,51 +30,125 @@ import time
 class LinearPipeline:
     # class constructor
     def __init__(self, N_train):
-        # variables instantiation 100
-        
+        # variables instantiation
         # Generating datasets
         self.N_train = N_train
-        self.x = np.arange(0, 1, 1/self.N_train)#np.random.rand(self.N_train)
-        self.y = np.arange(0, 1, 1/self.N_train)#np.exp(-self.x) - self.x**2 + 4 + 0.25 * np.random.randn(self.N_train)
-        #self.x, self.y = np.meshgrid(self.x, self.y)
-        #print(self.x, self.y)
-        
-    '''
-    Polynomial Regression - Main method of the class
-    '''
-    def doPolyRegression(self, *args):
+        #self.x = np.arange(0, 1, 1/self.N_train)#np.random.rand(self.N_train)
+        #self.y = np.arange(0, 1, 1/self.N_train)#np.exp(-self.x) - self.x**2 + 4 + 0.25 * np.random.randn(self.N_train)
+        self.x = np.sort(np.random.uniform(0, 1, N_train))
+        self.y = np.sort(np.random.uniform(0, 1, N_train))
 
-        # Getting the polynomial data (desing matrix)
-        X = self.generatePolynomial(args[0], args[1])
+    '''
+    Main method of the class - calculates everything, returns nothing
+    '''
+    def main(self, *args):
+        # the amount of variables (e.g. x,y => 2)
+        variables = args[0]
+        # the degre of polynomial we want to construct to match our data
+        degree    = args[1]
+        # kfold
+        kfold     = args[2]
+        # generating grid with the incoming data
         x, y = np.meshgrid(self.x, self.y)        
         # return z values and make it one dimensional (intead of 2 dimensional)
-        z = self.FrankeFunction(x, y) + 0.1 * np.random.randn(self.N_train)            
+        z = self.FrankeFunction(x, y)# + 0.1 * np.random.randn(self.N_train)          
         z_new = np.ravel(z)
-        # calculate betas
-        beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(z_new)
+
+        '''
+        (a) Ordinary Least Square on the Franke function with resampling:
+            Calling methond to do Linear Regression (with the generated polynomial)
+        '''
+        ''' MANUAL '''
+        # Generating polynomial to fit the data (and getting design matrix)
+        X = self.generatePolynomial(variables, degree)
+        # retrieving our model values
+        ztilde, beta = self.doPolyRegression(X, z_new)
+        ztilde = np.reshape(ztilde, (-1, self.N_train))
+        ''' SKLEARN '''
+        # values from scikit learn
+        # generate polynomial
+        poly_features = PolynomialFeatures(degree = degree, include_bias = False)
+        X_scikit = np.reshape((self.x, self.y), (self.N_train, variables))
+        #print(X_scikit)
+        X_poly = poly_features.fit_transform(X_scikit)
+        #print(len(X_poly))
+        lin_reg = LinearRegression().fit(X_poly, z)
+        beta_sk = lin_reg.coef_[0]
+        ztilde_sk = lin_reg.predict(X_poly)
+        # Printing out the values we are interested in
+        #print(ztilde[0], ztilde_sk[0])
+        print("betas are %s" %len(beta))
+        print("sklearn betas are %s" %len(beta_sk))
+        print("MSE is ", self.getMSE(z, ztilde), " and sklearn ", mean_squared_error(z, ztilde_sk))
+        print("R^2 is ", self.getR2(z, ztilde), " and sklearn ", lin_reg.score(X_poly, z))
+        # Plotting the resulting surface
+        fig = plt.figure(figsize = (10, 10))
+        # the last value is the subplot number => subplot(nrows, ncols, index, **kwargs)
+        ax1 = fig.add_subplot(2,2,1, projection='3d')
+        ax2 = fig.add_subplot(2,2,2, projection='3d')
+        #ax1.set_zlim3d(-0.2, 1.2)
+        surf1 = ax1.plot_surface(x, y, z, alpha=0.5, cmap = 'brg_r', linewidth = 0, antialiased = False)
+        ax1.scatter(x, y, ztilde, alpha=1, s=1, color='black')
+        #fig.colorbar(surf1, shrink=0.5, aspect=5)
+        surf2 = ax2.plot_surface(x, y, z, alpha=0.5, cmap = 'brg_r', linewidth = 0, antialiased = False)
+        ax2.scatter(x, y, ztilde_sk, alpha=1, s=1, color='black')
+        #fig.colorbar(surf2, shrink=0.5, aspect=5)
+        plt.show()
+        '''
+        (b) Resampling techniques:
+            k-fold cross validation
+        '''
+        ''' MANUAL '''
+        self.doKFoldCrossVal(X, z_new, kfold)
+        ''' SKLEARN '''
+        
+    '''
+    Polynomial Regression - does linear regression analysis with our generated polynomial
+    (should I add Singular Value Decomposition, as done in lecture notes?) and
+    returns the predicted values (our model)
+    '''
+    def doPolyRegression(self, *args):
+        # getting z values
+        z = args[1]
+        # Getting the polynomial data (desing matrix)
+        X = args[0]#self.generatePolynomial(args[0], args[1])
+        
+        # Applying Singular Value Decomposition
+        A = np.transpose(X) @ X
+        U, s, VT = np.linalg.svd(A)
+        D = np.zeros((len(U), len(VT)))
+        for i in range(0,len(VT)):
+            D[i,i] = s[i]
+        UT = np.transpose(U); 
+        V = np.transpose(VT); 
+        invD = np.linalg.inv(D)
+        invA = np.matmul(V, np.matmul(invD, UT))
+
+        # Calculate betas
+        beta = invA.dot(X.T).dot(z)
+        #beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(z)
+        #print("betas is ", beta)
         # and then make the prediction
         ztilde = X @ beta
-        # printing values - H/W (a)
-        print("MSE is ", self.MSE(z_new, ztilde))
-        print("R^2 is ", self.R2(z_new, ztilde))
         #z_new  = np.reshape(ztilde, (-1, 2))
         #print(z_new)
         #surf2 = ax.plot_surface(x, y, z_new, cmap = cm.coolwarm, linewidth = 0, antialiased = False)
         
         # Plot the surface.
-        fig = plt.figure()
-        ax  = fig.gca(projection='3d')
-        surf1 = ax.plot_surface(x, y, z, cmap = cm.coolwarm, linewidth = 0, antialiased = False)
+#        fig = plt.figure()
+#        ax  = fig.gca(projection='3d')
+#        surf1 = ax.plot_surface(x, y, z, cmap = cm.coolwarm, linewidth = 0, antialiased = False)
         # Add a color bar which maps values to colors.
-        fig.colorbar(surf1, shrink=0.5, aspect=5)
-        plt.show()
-        
-        # excersise (b)
-        self.doKFold(X, z_new)
+#        fig.colorbar(surf1, shrink=0.5, aspect=5)
+#        plt.show()
+        #print(beta)
+        #print("Confidence is ", self.getConfidence(X, invA, beta))
+        return ztilde, beta
         
     '''
     Generating polynomials for given number of variables for a given degree
-    using Newton Binomial formula, and when returning the list of all variables
+    using Newton's Binomial formula, and when returning the design matrix,
+    computed from the list of all variables
     '''
     def generatePolynomial(self, *args):
         # generating polynomial of 3 independent variables
@@ -90,6 +168,7 @@ class LinearPipeline:
         [pol.append(x[i]**j) for j in range(0, args[1]) for i in range(0, args[0])]
         # remove duplicates from the list
         mylist = list( dict.fromkeys(pol) )
+        #print(mylist)
         # we have 2 variables so, we need N^2 amount of points:
         # (for m variables we would have N^m points) 
         points = self.N_train**args[0]
@@ -112,23 +191,37 @@ class LinearPipeline:
         return term1 + term2 + term3 + term4
     
     '''
-    MSE
+    MSE - the smaller the better (0 is the best?)
     '''
-    def MSE(self, z_data, z_model):
+    def getMSE(self, z_data, z_model):
         n = np.size(z_model)
         return np.sum((z_data-z_model)**2)/n
     '''
-    R^2
+    R^2 - values should be between 0 and 1 (with 1 being the best)
     '''
-    def R2(self, z_data, z_model):
+    def getR2(self, z_data, z_model):
         return 1 - np.sum((z_data - z_model) ** 2) / np.sum((z_data - np.mean(z_data)) ** 2)
+    '''
+    Beta confidence interval
+    '''
+#    def getConfidence(self, X, matrix, beta, confidence = 0.95):
+#        weight = np.sqrt( np.diag( matrix ) ) * confidence
+#        betamin = beta - weight
+#        betamax = beta + weight
+#        return betamin, betamax
     
     # (b) splitting training and test set
-    def doKFold(self, *args):
-        X = args[0]
+    def doKFoldCrossVal(self, *args):
+        # getting z values
         z = args[1]
+        # Getting the polynomial data (desing matrix)
+        X = args[0]#self.generatePolynomial(args[0], args[1])
+        # getting the k value
+        kfold = args[2]
         # (b) splitting the data
         X_train, X_test, Z_train, Z_test = train_test_split(X, z, test_size = 0.2)
+        #print(np.random.randint(10, size=1), random.randrange(10))
+        # We calculate the size of each fold as the size of the dataset divided by the number of folds required.
 
 # The entry point of the program        
 if __name__ == '__main__':
@@ -140,10 +233,12 @@ if __name__ == '__main__':
     variables = 2
     # polynomial degree
     degree = 5
+    # kfold
+    kfold  = 10
     # object instantiation
     pipe = LinearPipeline(N_train)
     # calculating stuff
-    pipe.doPolyRegression(variables, degree)
+    pipe.main(variables, degree, kfold)#doPolyRegression(variables, degree)
     end_time = time.time()
     print("-- Script finished at %s sec --" %(end_time - start_time))
     
