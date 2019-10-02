9b31246a4af21f66074ba6a60e0fffb4841b6930
maksymbr <maxim.brilenkov@gmail.com>
2019-10-02 10:32:51 +0200

    Project-1: Small updates

--- p1/reglib.py
+++ p1/reglib.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 29 21:22:52 2019
-
-@author: maksymb
-"""
-
-
-
 import numpy as np
 # for polynimial manipulation
 import sympy as sp
-#from sympy import *
+# from sympy import *
 import itertools as it
 # Scikit learn utilities
 from sklearn.model_selection import train_test_split
@@ -21,6 +11,7 @@ class RegressionPipeline:
     '''
     class constructor
     '''
+
     def __init__(self, *args):
         # getting input values in place
         self.x_symb = args[0]
@@ -29,11 +20,12 @@ class RegressionPipeline:
     '''
     Franke function, used to generate outputs (z values)
     '''
+
     def FrankeFunction(self, x, y):
-        term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-        term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-        term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-        term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+        term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
+        term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
+        term3 = 0.5 * np.exp(-(9 * x - 7) ** 2 / 4.0 - 0.25 * ((9 * y - 3) ** 2))
+        term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
         return term1 + term2 + term3 + term4
 
     '''
@@ -41,20 +33,21 @@ class RegressionPipeline:
     using Newton's Binomial formula, and when returning the design matrix,
     computed from the list of all variables
     '''
+
     def constructDesignMatrix(self, *args):
         # the degree of polynomial to be generated
         poly_degree = args[0]
         # getting inputs
         x_vals = self.x_vals
-        # using itertools for generating all possible combinations 
+        # using itertools for generating all possible combinations
         # of multiplications between our variables and 1, i.e.:
-        # x_0*x_1*1, x_0*x_0*x_1*1 etc. => will get polynomial 
+        # x_0*x_1*1, x_0*x_0*x_1*1 etc. => will get polynomial
         # coefficients
         variables = list(self.x_symb.copy())
         variables.append(1)
         terms = [sp.Mul(*i) for i in it.combinations_with_replacement(variables, poly_degree)]
         # creating desing matrix
-        points = len(x_vals[0])*len(x_vals[1])
+        points = len(x_vals[0]) * len(x_vals[1])
         # creating desing matrix composed of ones
         X1 = np.ones((points, len(terms)))
         # populating design matrix with values
@@ -63,9 +56,11 @@ class RegressionPipeline:
             X1[:, k] = [f(i, j) for i in self.x_vals[1] for j in self.x_vals[0]]
         # returning constructed design matrix (for 2 approaches if needed)
         return X1
+
     '''
     Singular Value Decomposition for Linear Regression
     '''
+
     def doSVD(self, *args):
         # getting matrix
         X = args[0]
@@ -73,18 +68,19 @@ class RegressionPipeline:
         A = np.transpose(X) @ X
         U, s, VT = np.linalg.svd(A)
         D = np.zeros((len(U), len(VT)))
-        for i in range(0,len(VT)):
-            D[i,i] = s[i]
-        UT = np.transpose(U); 
-        V = np.transpose(VT); 
+        for i in range(0, len(VT)):
+            D[i, i] = s[i]
+        UT = np.transpose(U);
+        V = np.transpose(VT);
         invD = np.linalg.inv(D)
         invA = np.matmul(V, np.matmul(invD, UT))
-        
+
         return invA
-    
+
     '''
     k-Fold Cross Validation
     '''
+
     def doCrossVal(self, *args):
         # getting design matrix
         X = args[0]
@@ -92,7 +88,7 @@ class RegressionPipeline:
         z = np.ravel(args[1])
         kfold = args[2]
         # Splitting and shuffling data randomly
-        X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=1./kfold, shuffle = True)
+        X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=1. / kfold, shuffle=True)
         MSEtest_lintot = []
         MSEtrain_lintot = []
         z_tested = []
@@ -101,8 +97,8 @@ class RegressionPipeline:
             # Train The Pipeline
             invA = self.doSVD(X_train)
             beta_train = invA.dot(X_train.T).dot(z_train)
-            #invA = self.doSVD(X_test)
-            #beta_test = invA.dot(X_test.T).dot(z_test)
+            # invA = self.doSVD(X_test)
+            # beta_test = invA.dot(X_test.T).dot(z_test)
             # Testing the pipeline
             z_trained.append(X_train @ beta_train)
             z_tested.append(X_test @ beta_train)
@@ -114,7 +110,7 @@ class RegressionPipeline:
         MSEtrain_lin = np.mean(MSEtrain_lintot)
 
         return MSEtest_lin, MSEtrain_lin
-    
+
     def doCrossValRidge(self, *args):
         # getting design matrix
         X = args[0]
@@ -123,9 +119,9 @@ class RegressionPipeline:
         kfold = args[2]
         lambda_par = args[3]
         # Splitting and shuffling data randomly
-        X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=1./kfold, shuffle = True)
+        X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=1. / kfold, shuffle=True)
         # constructing the identity matrix
-        I = np.identity(len(X_train.T.dot(X_train)), dtype = float)
+        I = np.identity(len(X_train.T.dot(X_train)), dtype=float)
         MSEtest_ridgetot = []
         MSEtrain_ridgetot = []
         z_tested = []
@@ -135,8 +131,8 @@ class RegressionPipeline:
             # calculating parameters
             invA = np.linalg.inv(X_train.T.dot(X_train) + lambda_par * I)
             beta_train = invA.dot(X_train.T).dot(z_train)
-            #invA = self.doSVD(X_test)
-            #beta_test = invA.dot(X_test.T).dot(z_test)
+            # invA = self.doSVD(X_test)
+            # beta_test = invA.dot(X_test.T).dot(z_test)
             # Testing the pipeline
             z_trained.append(X_train @ beta_train)
             z_tested.append(X_test @ beta_train)
@@ -146,35 +142,39 @@ class RegressionPipeline:
         # Ridge MSE
         MSEtest_ridge = np.mean(MSEtest_ridgetot)
         MSEtrain_ridge = np.mean(MSEtrain_ridgetot)
-        
+
         return MSEtest_ridge, MSEtrain_ridge
-    
+
     ''' 
     Confidence intervals for beta
     '''
-#    def getConfidence(self, *args):
-        #beta, X, confidence=1.96
-#        beta = args[0]
-#        X = args[1]
-#        confidence = args[2]
-#        invA = self.doSVD(X)
-        
-#        weight = np.sqrt( np.diag( invA ) ) * confidence
-#        betamin = beta - weight
-#        betamax = beta + weight
-        
-#        return betamin, betamax
+    #    def getConfidence(self, *args):
+    # beta, X, confidence=1.96
+    #        beta = args[0]
+    #        X = args[1]
+    #        confidence = args[2]
+    #        invA = self.doSVD(X)
+
+    #        weight = np.sqrt( np.diag( invA ) ) * confidence
+    #        betamin = beta - weight
+    #        betamax = beta + weight
+
+    #        return betamin, betamax
     '''
     MSE - the smaller the better (0 is the best?)
     '''
+
     def getMSE(self, z_data, z_model):
         n = np.size(z_model)
-        return np.sum((z_data-z_model)**2)/n
+        return np.sum((z_data - z_model) ** 2) / n
+
     '''
     R^2 - values should be between 0 and 1 (with 1 being the best)
     '''
+
     def getR2(self, z_data, z_model):
         return 1 - np.sum((z_data - z_model) ** 2) / np.sum((z_data - np.mean(z_data)) ** 2)
+
     '''
     #============================#
     # Regression Methods
@@ -185,35 +185,38 @@ class RegressionPipeline:
     polynomial and returns the predicted values (our model) <= k-fold cross 
     validation has been implemented
     '''
+
     def doLinearRegression(self, *args):
         # getting design matrix
         X = args[0]
         # getting z values and making them 1d
         z = np.ravel(args[1])
         # calculating variance of data
-        
+
         # and then make the prediction
         invA = self.doSVD(X)
         beta = invA.dot(X.T).dot(z)
         ztilde = X @ beta
-        
+
         # calculating beta confidence
-        confidence = args[2] #1.96
-        sigma = np.var(z)#args[3] #1
+        confidence = args[2]  # 1.96
+        sigma = np.var(z)  # args[3] #1
         SE = sigma * np.sqrt(np.diag(invA)) * confidence
         beta_min = beta - SE
         beta_max = beta + SE
-        
-        return ztilde, beta, beta_min, beta_max#z_trained#ztilde#, beta, SE
+
+        return ztilde, beta, beta_min, beta_max  # z_trained#ztilde#, beta, SE
+
     '''
     Ridge Regression
     '''
+
     def doRidgeRegression(self, *args):
         # Generating the 500 values of lambda
         # to tune our model (i.e. we will calculate scores
         # and decide which parameter lambda is best suited for our model)
-        #nlambdas = 500
-        #lambdas = np.logspace(-3, 5, nlambdas)
+        # nlambdas = 500
+        # lambdas = np.logspace(-3, 5, nlambdas)
         # getting design matrix
         X = args[0]
         # getting z values
@@ -222,26 +225,27 @@ class RegressionPipeline:
         lambda_par = args[2]
         # constructing the identity matrix
         XTX = X.T.dot(X)
-        I = np.identity(len(XTX), dtype = float)
+        I = np.identity(len(XTX), dtype=float)
         # calculating parameters
         invA = np.linalg.inv(XTX + lambda_par * I)
         beta = invA.dot(X.T).dot(z)
         # and making predictions
         ztilde = X @ beta
-        
+
         # calculating beta confidence
-        confidence = args[3] #1.96
+        confidence = args[3]  # 1.96
         # calculating variance
-        sigma = np.var(z)#args[4] #1
+        sigma = np.var(z)  # args[4] #1
         SE = sigma * np.sqrt(np.diag(invA)) * confidence
         beta_min = beta - SE
         beta_max = beta + SE
 
-        return ztilde, beta, beta_min, beta_max#, beta, SE
+        return ztilde, beta, beta_min, beta_max  # , beta, SE
+
     '''
     LASSO Regression
     '''
+
     def doLASSORegression(self, *args):
         pass
-    
-    
+
--- p1/regression.py
+++ p1/regression.py
@@ -6,20 +6,20 @@ Created on Thu Sep 26 16:13:27 2019
 @author: maksymb
 """
 
+# standard imports
 import numpy as np
-# for polynimial manipulation
+# for polynomial manipulation
 import sympy as sp
-#from sympy import *
+# from sympy import *
 import itertools as it
 # importing my library
 import reglib as rl
-
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 # to use latex symbols
-#from matplotlib import rc
+# from matplotlib import rc
 
 # to read tif files
 from imageio import imread
@@ -36,24 +36,9 @@ from sklearn.model_selection import KFold
 
 import time
 
+# rc('text', usetex=True)
+# rc('text.latex', preamble=r'\usepackage{amssymb}')
 
-
-#rc('text', usetex=True)
-#rc('text.latex', preamble=r'\usepackage{amssymb}')
-    
-    
-    
-    
-# Initialising global variables 
-# (I know it is not the best way to do things in python, but let it be for now)
-kFoldMSEtest_lin    = []
-kFoldMSEtrain_lin   = []
-kFoldMSEtest_ridge  = []
-kFoldMSEtrain_ridge = []
-kFoldMSEtest_lasso  = []
-    
-    
-    
 class MainPipeline:
     ''' class constructor '''
     def __init__(self, *args):
@@ -68,17 +53,18 @@ class MainPipeline:
         # to calculate beta confidence intervals
         self.confidence = args[4]
         self.sigma = args[5]
-        # hyperparameter
+        # hyper parameter
         self.lambda_par = args[6]
-    
+
     '''
     method to work with fake data, i.e. created using uniform distribution
     '''
+
     def doFakeData(self, *args):
         # creating a starting value for number generator
         # so each time we will get the same random numbers
         np.random.seed(1)
-        # generating an array of symbolic variables 
+        # generating an array of symbolic variables
         # based on the desired amount of variables
         self.x_symb = sp.symarray('x', self.n_vars, real=True)
         # making a copy of this array
@@ -94,7 +80,7 @@ class MainPipeline:
         z = lib.FrankeFunction(x, y) + 0.1 * np.random.randn(self.N, self.N)
         # getting design matrix
         X = lib.constructDesignMatrix(self.poly_degree)
-        
+
         ''' Linear Regression '''
         ''' MANUAL '''
         # getting predictions
@@ -102,7 +88,7 @@ class MainPipeline:
         ztilde_lin = ztilde_lin.reshape(-1, self.N)
         ''' Scikit Learn '''
         # generate polynomial
-        poly_features = PolynomialFeatures(degree = self.poly_degree)
+        poly_features = PolynomialFeatures(degree=self.poly_degree)
         # [[x[0], y[0]],[x[1],y[1]], [x[2],y[2]], ...]
         # works mush better than the transpose and reshape
         # (so far reshape, without "F" order was giving crap)
@@ -112,102 +98,102 @@ class MainPipeline:
         lin_reg = LinearRegression().fit(X_poly, np.ravel(z))
         ztilde_sk = lin_reg.predict(X_poly).reshape(-1, self.N)
         zarray_lin = [z.reshape(-1, self.N), ztilde_lin, ztilde_sk]
-#        print('\n')
-#        print("Linear MSE (no CV) - " + str(lib.getMSE(z, ztilde_lin)) + "; sklearn - " + str(mean_squared_error(z, ztilde_sk)))
-#        print("Linear R^2 (no CV) - " + str(lib.getR2(z, ztilde_lin)) + "; sklearn - " + str(lin_reg.score(X_poly, z)))
-#        print('\n')
+        #        print('\n')
+        #        print("Linear MSE (no CV) - " + str(lib.getMSE(z, ztilde_lin)) + "; sklearn - " + str(mean_squared_error(z, ztilde_sk)))
+        #        print("Linear R^2 (no CV) - " + str(lib.getR2(z, ztilde_lin)) + "; sklearn - " + str(lin_reg.score(X_poly, z)))
+        #        print('\n')
         ''' Plotting Surfaces '''
-        fig = plt.figure(figsize = (10, 3))
+        fig = plt.figure(figsize=(10, 3))
         fig.suptitle('Linear Regression', fontsize=14)
         axes = [fig.add_subplot(1, 3, i, projection='3d') for i in range(1, 4)]
         surf = [axes[i].plot_surface(x, y, zarray_lin[i], alpha=0.5, \
-                cmap = 'brg_r', linewidth = 0, antialiased = False) for i in range(3)]
+                                     cmap='brg_r', linewidth=0, antialiased=False) for i in range(3)]
         # betas
-        fig = plt.figure(figsize = (10, 3))
+        fig = plt.figure(figsize=(10, 3))
         ax1 = fig.add_subplot(1, 1, 1)
         t = []
-        [t.append(i) for i in range(1, len(beta_lin)+1)]
-        ax1.plot(t, beta_lin, 'bo', label = r'$\beta$')
-        ax1.plot(t, beta_min, 'r--', label = r'$\beta_{min}$')
-        ax1.plot(t, beta_max, 'g--', label = r'$\beta_{max}$')
+        [t.append(i) for i in range(1, len(beta_lin) + 1)]
+        ax1.plot(t, beta_lin, 'bo', label=r'$\beta$')
+        ax1.plot(t, beta_min, 'r--', label=r'$\beta_{min}$')
+        ax1.plot(t, beta_max, 'g--', label=r'$\beta_{max}$')
         ax1.legend()
         plt.grid(True)
         plt.xlabel('number of ' + r'$\beta$')
         plt.ylabel(r'$\beta$')
 
         # Calculating k-Fold Cross Validation
-        global kFoldMSEtest_lin, kFoldMSEtrain_lin
-        kFoldMSEtest_lin.append(lib.doCrossVal(X, z, self.kfold)[0])
-        kFoldMSEtrain_lin.append(lib.doCrossVal(X, z, self.kfold)[1])
-        
+        self.kFoldMSEtest_lin = lib.doCrossVal(X, z, self.kfold)[0]
+        self.kFoldMSEtrain_lin = lib.doCrossVal(X, z, self.kfold)[1]
+
         ''' Ridge Regression '''
         ''' MANUAL '''
-        ztilde_ridge, beta_ridge, beta_min, beta_max = lib.doRidgeRegression(X, z, self.lambda_par, self.confidence, self.sigma)
+        ztilde_ridge, beta_ridge, beta_min, beta_max = lib.doRidgeRegression(X, z, self.lambda_par, self.confidence,
+                                                                             self.sigma)
         ztilde_ridge = ztilde_ridge.reshape(-1, self.N)
         ''' Scikit Learn '''
-        ridge_reg = Ridge(alpha = self.lambda_par, fit_intercept = True).fit(X_poly, np.ravel(z))
+        ridge_reg = Ridge(alpha=self.lambda_par, fit_intercept=True).fit(X_poly, np.ravel(z))
         ztilde_sk = ridge_reg.predict(X_poly).reshape(-1, self.N)
         zarray_ridge = [z.reshape(-1, self.N), ztilde_ridge, ztilde_sk]
-#        print('\n')
-#        print("Ridge MSE (no CV) - " + str(lib.getMSE(z, ztilde_ridge)) + "; sklearn - " + str(mean_squared_error(z, ztilde_sk)))
-#        print("Ridge R^2 (no CV) - " + str(lib.getR2(z, ztilde_ridge)) + "; sklearn - " + str(ridge_reg.score(X_poly, z)))
-#        print('\n')
+        #        print('\n')
+        #        print("Ridge MSE (no CV) - " + str(lib.getMSE(z, ztilde_ridge)) + "; sklearn - " + str(mean_squared_error(z, ztilde_sk)))
+        #        print("Ridge R^2 (no CV) - " + str(lib.getR2(z, ztilde_ridge)) + "; sklearn - " + str(ridge_reg.score(X_poly, z)))
+        #        print('\n')
         ''' Plotting Surfaces '''
-        fig = plt.figure(figsize = (10, 3))
+        fig = plt.figure(figsize=(10, 3))
         fig.suptitle('Ridge Regression', fontsize=14)
         axes = [fig.add_subplot(1, 3, i, projection='3d') for i in range(1, 4)]
-        surf = [axes[i].plot_surface(x, y, zarray_ridge[i], alpha=0.5,\
-                cmap = 'brg_r', linewidth = 0, antialiased = False) for i in range(3)]
-                # betas
+        surf = [axes[i].plot_surface(x, y, zarray_ridge[i], alpha=0.5, \
+                                     cmap='brg_r', linewidth=0, antialiased=False) for i in range(3)]
+        # betas
         print('\n')
-        fig = plt.figure(figsize = (10, 3))
+        fig = plt.figure(figsize=(10, 3))
         ax1 = fig.add_subplot(1, 1, 1)
         t = []
-        [t.append(i) for i in range(1, len(beta_lin)+1)]
-        ax1.plot(t, beta_ridge, 'bo', label = r'$\beta$')
-        ax1.plot(t, beta_min, 'r--', label = r'$\beta_{min}$')
-        ax1.plot(t, beta_max, 'g--', label = r'$\beta_{max}$')
+        [t.append(i) for i in range(1, len(beta_lin) + 1)]
+        ax1.plot(t, beta_ridge, 'bo', label=r'$\beta$')
+        ax1.plot(t, beta_min, 'r--', label=r'$\beta_{min}$')
+        ax1.plot(t, beta_max, 'g--', label=r'$\beta_{max}$')
         ax1.legend()
         plt.grid(True)
         plt.xlabel('number of ' + r'$\beta$')
         plt.ylabel(r'$\beta$')
         print('\n')
         # Calculating k-Fold Cross Validation
-        global kFoldMSEtest_ridge, kFoldMSEtrain_ridge
-        kFoldMSEtest_ridge.append(lib.doCrossValRidge(X, z, self.kfold, self.lambda_par)[0])
-        kFoldMSEtrain_ridge.append(lib.doCrossValRidge(X, z, self.kfold, self.lambda_par)[1])
-        
+        self.kFoldMSEtest_ridge = lib.doCrossValRidge(X, z, self.kfold, self.lambda_par)[0]
+        self.kFoldMSEtrain_ridge = lib.doCrossValRidge(X, z, self.kfold, self.lambda_par)[1]
+
         ''' LASSO Regression '''
-        lasso_reg = Lasso(alpha = self.lambda_par).fit(X_poly, np.ravel(z))
+        lasso_reg = Lasso(alpha=self.lambda_par).fit(X_poly, np.ravel(z))
         ztilde_sk = lasso_reg.predict(X_poly).reshape(-1, self.N)
         zarray_lasso = [z.reshape(-1, self.N), ztilde_sk]
-#        print('\n')
-#        print("SL Lasso MSE (no CV) - " + str(mean_squared_error(z, ztilde_sk)))
-#        print("SL Lasso R^2 (no CV) - " + str(lasso_reg.score(X_poly, z)))
-#        print('\n')
+        #        print('\n')
+        #        print("SL Lasso MSE (no CV) - " + str(mean_squared_error(z, ztilde_sk)))
+        #        print("SL Lasso R^2 (no CV) - " + str(lasso_reg.score(X_poly, z)))
+        #        print('\n')
         ''' Plotting Surfaces '''
-        fig = plt.figure(figsize = (10, 3))
+        fig = plt.figure(figsize=(10, 3))
         fig.suptitle('Lasso Regression', fontsize=14)
         axes = [fig.add_subplot(1, 3, i, projection='3d') for i in range(1, 3)]
-        surf = [axes[i].plot_surface(x, y, zarray_lasso[i], alpha=0.5,\
-                cmap = 'brg_r', linewidth = 0, antialiased = False) for i in range(2)]
-        
+        surf = [axes[i].plot_surface(x, y, zarray_lasso[i], alpha=0.5, \
+                                     cmap='brg_r', linewidth=0, antialiased=False) for i in range(2)]
+
         plt.show()
-        
+
         # Calculating k-Fold Cross Validation
-        global kFoldMSE_lasso
-        
+        #global kFoldMSE_lasso
+
     '''
     method to work with REAL data, i.e. downloaded from web
     '''
+
     def doRealData(self, *args):
         pass
-    
-    
+
+
 if __name__ == '__main__':
     # Start time of the program
     start_time = time.time()
-    
+
     ''' Working with Fake Data '''
     ''' Input Parameters '''
     # number of points
@@ -225,47 +211,65 @@ if __name__ == '__main__':
     lambda_par = 0.000001
     # object class instantiation
     print('Fake Data')
-    for poly_degree in range(1, max_poly_degree+1):
+
+    kFoldMSEtest_lin = []
+    kFoldMSEtrain_lin = []
+    kFoldMSEtest_ridge = []
+    kFoldMSEtrain_ridge = []
+    kFoldMSEtest_lasso = []
+
+    for poly_degree in range(1, max_poly_degree + 1):
         print('\n')
         print('Starting analysis for polynomial of degree: ' + str(poly_degree))
-        pipeline = MainPipeline(N_points, n_vars, poly_degree,\
-                            kfold, confidence, sigma, lambda_par)
+        pipeline = MainPipeline(N_points, n_vars, poly_degree,
+                                kfold, confidence, sigma, lambda_par)
         # Linear regression on fake data
         pipeline.doFakeData()
-    
+        # linear regression kfold
+        kFoldMSEtest_lin.append(pipeline.kFoldMSEtest_lin)
+        kFoldMSEtrain_lin.append(pipeline.kFoldMSEtrain_lin)
+        # ridge regression kfold
+        kFoldMSEtest_ridge.append(pipeline.kFoldMSEtest_ridge)
+        kFoldMSEtrain_ridge.append(pipeline.kFoldMSEtrain_ridge)
+
     ''' MSE as a function of model complexity '''
     # plotting MSE from test data
     fig = plt.figure(figsize = (10, 3))
     ax1 = fig.add_subplot(1, 1, 1)
     t = []
-    [t.append(i) for i in range(1, max_poly_degree+1)]
-    ax1.plot(t, kFoldMSEtest_lin, 'bo', label = 'test')
-    ax1.plot(t, kFoldMSEtrain_lin, 'r--', label = 'train')
+    [t.append(i) for i in range(1, max_poly_degree + 1)]
+    ax1.plot(t, kFoldMSEtest_lin, 'bo', label='test')
+    ax1.plot(t, kFoldMSEtrain_lin, 'r--', label='train')
     ax1.set_yscale('log')
     ax1.legend()
     plt.grid(True)
     plt.title('MSE as a function of model complexity; Linear Regression')
     plt.xlabel('model complexity (polynomial degree)')
     plt.ylabel('MSE')
-    
-    fig = plt.figure(figsize = (10, 3))
+
+    fig = plt.figure(figsize=(10, 3))
     ax1 = fig.add_subplot(1, 1, 1)
     t = []
-    [t.append(i) for i in range(1, max_poly_degree+1)]
-    ax1.plot(t, kFoldMSEtest_ridge, 'bo', label = 'test')
-    ax1.plot(t, kFoldMSEtrain_ridge, 'r--', label = 'train')
+    [t.append(i) for i in range(1, max_poly_degree + 1)]
+    ax1.plot(t, kFoldMSEtest_ridge, 'bo', label='test')
+    ax1.plot(t, kFoldMSEtrain_ridge, 'r--', label='train')
     ax1.legend()
     plt.grid(True)
     plt.title('MSE as a function of model complexity; Ridge Regression')
     plt.xlabel('model complexity (polynomial degree)')
     plt.ylabel('MSE')
     plt.show()
-    
+
     ''' Working with Real Data '''
+    '''
+    In this way we are getting Z values, now what I need is to generate X,Y
+    using linspace or similar features
+    '''
     print('\n')
     print('Real Data')
     # Load the terrain
-    terrain1 = imread('Data/SRTM_data_Norway_1.tif') # <= getting z values, now I need to create my x and y with np.linspace
+    terrain1 = imread(
+        'Data/SRTM_data_Norway_1.tif')  # <= getting z values, now I need to create my x and y with np.linspace
     print(np.shape(terrain1))
     # Show the terrain
     plt.figure()
@@ -274,20 +278,19 @@ if __name__ == '__main__':
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.show()
-    
+
     # End time of the program
     end_time = time.time()
-    print("-- Program finished at %s sec --" %(end_time - start_time))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    print("-- Program finished at %s sec --" % (end_time - start_time))
+
+
+
+
+
+
+
+
+
+
+
+
