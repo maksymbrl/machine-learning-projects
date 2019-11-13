262462094cafad1dfb7fb0675e8bf1a20d1d0859
maksymbr <maxim.brilenkov@gmail.com>
2019-11-13 09:47:19 +0100

    Adding skeletons to occupy space.

---
+++ p2/code/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# this is the main module for project 2
+class MainPipeline:
+    __init__(self, *args):
+        pass
+
+    neural(self, *args):
+        print("Activate Neural Network")
+
+    logistic(self, *args):
+        print("stuff for logictic regression")
+
+if __name__ == '__main__':
+    pipe = MainPipeline()
